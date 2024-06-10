4cbf6fb3fc7d4c336875b8fc61753ad2ef70453b
IanM07 <93929476+IanM07@users.noreply.github.com>
2024-06-10 11:51:27 -0400

    Processing Numerical and Alphanumerical Values
    Added logic to parse the values and output either the numerical or alphanumerical values depending on how the user calls the script.

--- MyApp.ps1
+++ MyApp.ps1
@@ -11,10 +11,31 @@ $fileContent = Get-Content -Path $FileName
 #Split the file contents by commas and store them in an array so we can parse each value
 $values = $fileContent -split ','
 
+#Split the values into numeric and alphabetical lists
+$numericValues = $values | Where-Object { 
+    #Trim any leading or trailing whitespace
+    $_ = $_.Trim()
+
+    #Ensure the value is numeric
+    ($_ -match "^[0-9eE.-]+$") -and (-not ($_ -match "^'.*'$"))
+}
+
+$alphaValues = $values | Where-Object { 
+    #Trim any leading or trailing whitespace
+    $_ = $_.Trim()
+
+    #Ensure the value is alphanumeric
+    ($_ -match "^'.*'$") -or ($_ -match "^[A-Za-z]+$")
+}
+
+
 #If the DataType is numeric then convert each value to an int
 if ($DataType -eq "numeric"){
-    $values = $numbers | ForEach-Object {[float]$_}
+    $values = $numericValues | ForEach-Object {[float]$_}
+}elseif ($DataType -eq "alpha"){
+    $values = $alphaValues | ForEach-Object {[string]$_}
 }
+
 
 #Sort the values either ascending or descending based on the value specified by SortOrder
 if ($SortOrder -eq "ascending") {
