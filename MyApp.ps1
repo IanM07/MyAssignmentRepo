85e50247eb66aef4d3991f7bf74f02d659a0aab2
IanM07 <93929476+IanM07@users.noreply.github.com>
2024-06-10 12:38:28 -0400

    Assignment Requirements Met
    Finished logic to meet the assignment requirements. The script can now process both numeric and alphanumeric lists of text and then combine them into one final list for output if need be.

--- MyApp.ps1
+++ MyApp.ps1
@@ -5,28 +5,24 @@ param (
     [string]$SortOrder
 )
 
-#Read the content of the file
+#Read the contents of the file
 $fileContent = Get-Content -Path $FileName
 
-#Split the file contents by commas and store them in an array in order to parse each value
+#Split the file contents by commas and store them in an array
 $values = $fileContent -split ','
 
 #Split the values into a numeric list
 $numericValues = $values | Where-Object { 
-    #Trim any leading or trailing whitespace
+    #Trim any leading or trailing whitespace then ensure the value is numeric
     $_ = $_.Trim()
-
-    #Ensure the value is numeric
     ($_ -match "^[0-9eE.-]+$") -and (-not ($_ -match "^'.*'$"))
 }
 
 
 #Split the values into an alphanumeric list
 $alphaValues = $values | Where-Object { 
-    #Trim any leading or trailing whitespace
+    #Trim any leading or trailing whitespace then ensure the value is alphanumeric
     $_ = $_.Trim()
-
-    #Ensure the value is alphanumeric
     ($_ -match "^'.*'$") -or ($_ -match "^[A-Za-z]+$")
 }
 
@@ -38,16 +34,43 @@ if ($DataType -eq "numeric"){
 #If the DataType is alphanumeric then convert each value to a string
 }elseif ($DataType -eq "alpha"){
     $values = $alphaValues | ForEach-Object {[string]$_}
+
+#If the DataType is both then convert both lists accordingly
+}elseif ($DataType -eq "both") {
+    $numericValues = $numericValues | ForEach-Object { [decimal]$_ }
+    $alphaValues = $alphaValues | ForEach-Object { [string]$_ }
 }
 
 
-#Sort the values in ascending order
-if ($SortOrder -eq "ascending") {
-    $values = $values | Sort-Object
 
-#Sort the values in descending order
-} elseif ($SortOrder -eq "descending") {
-    $values = $values | Sort-Object -Descending
+#If the DataType is both, sort both lists
+if ($DataType -eq "both") {
+    $numericValues = $numericValues | ForEach-Object { [decimal]$_ }
+    $alphaValues = $alphaValues | ForEach-Object { [string]$_ }
+
+    # Sort both lists
+    if ($SortOrder -eq "ascending") {
+        $numericValues = $numericValues | Sort-Object
+        $alphaValues = $alphaValues | Sort-Object
+
+    } elseif ($SortOrder -eq "descending") {
+        $numericValues = $numericValues | Sort-Object -Descending
+        $alphaValues = $alphaValues | Sort-Object -Descending
+    }
+
+    #Concatenate the two lists
+    $values = $numericValues + $alphaValues
+
+#If the DataType is not both then sort the list accordingly
+}else{
+    #Sort the values in ascending order
+    if ($SortOrder -eq "ascending") {
+        $values = $values | Sort-Object
+
+    #Sort the values in descending order
+    }elseif ($SortOrder -eq "descending") {
+        $values = $values | Sort-Object -Descending
+    }
 }
 
 
