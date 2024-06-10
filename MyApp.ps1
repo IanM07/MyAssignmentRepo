567053c7ab1238a1403af2cd1ae36fb6954b9d7a
IanM07 <93929476+IanM07@users.noreply.github.com>
2024-06-10 12:17:02 -0400

    Bug Fix and Comments
    Fixed the small bug where scientific notation would still be displayed as scientific notation instead of a decimal.

--- MyApp.ps1
+++ MyApp.ps1
@@ -8,10 +8,10 @@ param (
 #Read the content of the file
 $fileContent = Get-Content -Path $FileName
 
-#Split the file contents by commas and store them in an array so we can parse each value
+#Split the file contents by commas and store them in an array in order to parse each value
 $values = $fileContent -split ','
 
-#Split the values into numeric and alphabetical lists
+#Split the values into a numeric list
 $numericValues = $values | Where-Object { 
     #Trim any leading or trailing whitespace
     $_ = $_.Trim()
@@ -20,6 +20,8 @@ $numericValues = $values | Where-Object {
     ($_ -match "^[0-9eE.-]+$") -and (-not ($_ -match "^'.*'$"))
 }
 
+
+#Split the values into an alphanumeric list
 $alphaValues = $values | Where-Object { 
     #Trim any leading or trailing whitespace
     $_ = $_.Trim()
@@ -29,23 +31,26 @@ $alphaValues = $values | Where-Object {
 }
 
 
-#If the DataType is numeric then convert each value to an int
+#If the DataType is numeric then convert each value to a decimal
 if ($DataType -eq "numeric"){
-    $values = $numericValues | ForEach-Object {[float]$_}
+    $values = $numericValues | ForEach-Object {[decimal]$_}
+
+#If the DataType is alphanumeric then convert each value to a string
 }elseif ($DataType -eq "alpha"){
     $values = $alphaValues | ForEach-Object {[string]$_}
 }
 
 
-#Sort the values either ascending or descending based on the value specified by SortOrder
+#Sort the values in ascending order
 if ($SortOrder -eq "ascending") {
     $values = $values | Sort-Object
+
+#Sort the values in descending order
 } elseif ($SortOrder -eq "descending") {
     $values = $values | Sort-Object -Descending
 }
 
-#Rejoin the sorted values into a comma-separated list
-$output = $values -join ","
 
-#Output our final list of sorted values
+#Rejoin the sorted values into a comma-separated list and output the final list
+$output = $values -join ","
 Write-Output $output
